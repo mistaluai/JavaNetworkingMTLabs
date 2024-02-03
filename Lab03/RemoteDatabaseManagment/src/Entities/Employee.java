e091a92d05f8d92fa7e2f10df46e60f7b870be1c
Luai Waleed <mistaluai@gmail.com>
2024-02-03 02:55:38 +0200

    finished implementing the client class and the remote connection handling

--- Lab03/RemoteDatabaseManagment/src/Entities/Employee.java
+++ Lab03/RemoteDatabaseManagment/src/Entities/Employee.java
@@ -1,6 +1,8 @@
 package Entities;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable {
     private String name;
     private int ID;
 
--- Lab03/RemoteDatabaseManagment/src/RemoteConnectionHandling/Client.java
+++ Lab03/RemoteDatabaseManagment/src/RemoteConnectionHandling/Client.java
@@ -1,7 +1,44 @@
 package RemoteConnectionHandling;
 
-public class Client {
-    public static void main(String[] args) {
+import Entities.Employee;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+public class Client {
+    IServer server;
+    Client() throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry(1099);
+        server = (IServer) registry.lookup("Server");
+    }
+    public boolean add(Employee employee) throws RemoteException {
+        return server.add(employee);
+    }
+
+    public boolean update(int ID, Employee newEmployee) throws RemoteException {
+        return server.update(ID, newEmployee);
+    }
+
+    public boolean delete(int ID) throws RemoteException {
+        return server.delete(ID);
+    }
+
+    public Employee get(int ID) throws RemoteException {
+        return server.get(ID);
+    }
+
+    public String[][] getEmployees() throws RemoteException {
+        List<Employee> employeesList = server.getEmployees();
+        String[][] employeesArray = new String[employeesList.size()][2];
+        int index = 0;
+        for (Employee e : employeesList) {
+            employeesArray[index][0] = String.valueOf(e.getID());
+            employeesArray[index][1] = e.getName();
+            index++;
+        }
+        return employeesArray;
     }
 }
